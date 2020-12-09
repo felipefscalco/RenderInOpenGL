b022edb69633ef04b0388466ae2a38895f9197dc
Cristian Burile <cristianburile@hotmail.com>
2020-12-09 02:15:15 -0300

    Added constant on some variables

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -13,40 +13,40 @@
 
 typedef struct
 {
-	unsigned int VAO;
-	unsigned int VBO;
+	unsigned int vao;
+	unsigned int vbo;
 	unsigned int texture;
 	int points;
-	bool drawTexture;
-} CustomObject;
+	bool draw_texture;
+} custom_object;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow* window);
-CustomObject load_custom_object(std::pair<std::string, std::string> fileNameAndTexture);
-unsigned int load_object_texture(std::string textureFileName);
+void process_input(GLFWwindow* window);
+custom_object load_custom_object(const std::pair<std::string, std::string>& file_name_and_texture);
+unsigned int load_object_texture(const std::string& texture_file_name);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-const unsigned int VERTICE_DEFINITION = 11; //3 Positions + 3 Colors + 3 Normal Vector + 2 Texture Coordinates
+const unsigned int scr_width = 800;
+const unsigned int scr_height = 600;
+const unsigned int vertice_definition = 11; //3 Positions + 3 Colors + 3 Normal Vector + 2 Texture Coordinates
 
 // camera
 Camera camera(glm::vec3(0.0f, 0.0f, 6.0f));
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
+float last_x = scr_width / 2.0f;
+float last_y = scr_height / 2.0f;
+bool first_mouse = true;
 
 // timing
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
+float delta_time = 0.0f;
+float last_frame = 0.0f;
 
 // lighting
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 light_pos(1.2f, 1.0f, 2.0f);
 
 // specular reflex
-float specularStrength = 0.5;
+float specular_strength = 0.5;
 
 int main()
 {
@@ -57,14 +57,14 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// glfw window creation
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "CSV Renderer", NULL, NULL);
-	if (window == NULL)
+	auto* const window = glfwCreateWindow(scr_width, scr_height, "CSV Renderer", nullptr, nullptr);
+	if (window == 0)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-
+	
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetCursorPosCallback(window, mouse_callback);
@@ -83,7 +83,7 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 	// build and compile our shader zprogram
-	Shader lightingShader("src/shaders/phong_lighting.vs", "src/shaders/phong_lighting.fs");
+	Shader lighting_shader("src/shaders/phong_lighting.vs", "src/shaders/phong_lighting.fs");
 
 	Shader lightCubeShader("src/shaders/light_cube.vs", "src/shaders/light_cube.fs");
 
@@ -97,57 +97,57 @@ int main()
 		{"src/resources/rooftop.csv", "src/textures/rooftop.jpg"}
 	};
 
-	int modelsAndTexturesCount = sizeof(modelsAndTextures) / sizeof(modelsAndTextures[0]);
-	CustomObject* customObjects = new CustomObject[modelsAndTexturesCount];
-	for (int i = 0; i < modelsAndTexturesCount; i++)
-		customObjects[i] = load_custom_object(modelsAndTextures[i]);
+	const int models_and_textures_count = sizeof(modelsAndTextures) / sizeof(modelsAndTextures[0]);
+	auto* custom_objects = new custom_object[models_and_textures_count];
+	for (auto i = 0; i < models_and_textures_count; i++)
+		custom_objects[i] = load_custom_object(modelsAndTextures[i]);
 
-	CustomObject sun = load_custom_object({ "src/resources/sun.csv", "" });
+	auto sun = load_custom_object({ "src/resources/sun.csv", "" });
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// per-frame time logic
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
+		const float current_frame = glfwGetTime();
+		delta_time = current_frame - last_frame;
+		last_frame = current_frame;
 
 		// input
-		processInput(window);
+		process_input(window);
 
 		// render
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
-		lightPos.x = sin(glfwGetTime()) * 2.0f;
-		lightPos.y = sin(glfwGetTime()) * 2.0f;
-		lightPos.z = cos(glfwGetTime()) * 2.0f;
+		light_pos.x = sin(glfwGetTime()) * 2.0f;
+		light_pos.y = sin(glfwGetTime()) * 2.0f;
+		light_pos.z = cos(glfwGetTime()) * 2.0f;
 
 		// be sure to activate shader when setting uniforms/drawing objects
-		lightingShader.use();
-		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-		lightingShader.setVec3("lightPos", lightPos);
-		lightingShader.setVec3("viewPos", camera.Position);
-		lightingShader.setFloat("specularStrength", specularStrength);
+		lighting_shader.use();
+		lighting_shader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lighting_shader.setVec3("lightPos", light_pos);
+		lighting_shader.setVec3("viewPos", camera.Position);
+		lighting_shader.setFloat("specularStrength", specular_strength);
 
 		// view/projection transformations
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		glm::mat4 view = camera.GetViewMatrix();
-		lightingShader.setMat4("projection", projection);
-		lightingShader.setMat4("view", view);
+		auto projection = glm::perspective(glm::radians(camera.Zoom), static_cast<float>(scr_width) / static_cast<float>(scr_height), 0.1f, 100.0f);
+		auto view = camera.GetViewMatrix();
+		lighting_shader.setMat4("projection", projection);
+		lighting_shader.setMat4("view", view);
 
 		// world transformation
-		glm::mat4 model = glm::mat4(1.0f);
-		lightingShader.setMat4("model", model);
+		auto model = glm::mat4(1.0f);
+		lighting_shader.setMat4("model", model);
 
 		// render objects
-		for (int i = 0; i < modelsAndTexturesCount; i++)
+		for (auto i = 0; i < models_and_textures_count; i++)
 		{
-			glBindTexture(GL_TEXTURE_2D, customObjects[i].texture);
-			lightingShader.setBool("drawTexture", customObjects[i].drawTexture);
-			glBindVertexArray(customObjects[i].VAO);
-			glDrawArrays(GL_TRIANGLES, 0, customObjects[i].points);
+			glBindTexture(GL_TEXTURE_2D, custom_objects[i].texture);
+			lighting_shader.setBool("drawTexture", custom_objects[i].draw_texture);
+			glBindVertexArray(custom_objects[i].vao);
+			glDrawArrays(GL_TRIANGLES, 0, custom_objects[i].points);
 		}
 
 		// also draw the lamp object
@@ -155,11 +155,11 @@ int main()
 		lightCubeShader.setMat4("projection", projection);
 		lightCubeShader.setMat4("view", view);
 		model = glm::mat4(1.0f);
-		model = glm::translate(model, lightPos);
-		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		model = translate(model, light_pos);
+		model = scale(model, glm::vec3(0.2f)); // a smaller cube
 		lightCubeShader.setMat4("model", model);
 
-		glBindVertexArray(sun.VAO);
+		glBindVertexArray(sun.vao);
 		glDrawArrays(GL_TRIANGLES, 0, sun.points);
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -168,14 +168,14 @@ int main()
 	}
 
 	// optional: de-allocate all resources once they've outlived their purpose:
-	for (int i = 0; i < modelsAndTexturesCount; i++)
+	for (auto i = 0; i < models_and_textures_count; i++)
 	{
-		glDeleteVertexArrays(1, &customObjects[i].VAO);
-		glDeleteBuffers(1, &customObjects[i].VBO);
+		glDeleteVertexArrays(1, &custom_objects[i].vao);
+		glDeleteBuffers(1, &custom_objects[i].vbo);
 	}
 
-	glDeleteVertexArrays(1, &sun.VAO);
-	glDeleteBuffers(1, &sun.VBO);
+	glDeleteVertexArrays(1, &sun.vao);
+	glDeleteBuffers(1, &sun.vbo);
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	glfwTerminate();
@@ -183,37 +183,37 @@ int main()
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-void processInput(GLFWwindow* window)
+void process_input(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, deltaTime);
+		camera.ProcessKeyboard(FORWARD, delta_time);
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
+		camera.ProcessKeyboard(BACKWARD, delta_time);
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, deltaTime);
+		camera.ProcessKeyboard(LEFT, delta_time);
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, deltaTime);
+		camera.ProcessKeyboard(RIGHT, delta_time);
 	if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
 	{
-		specularStrength += 0.01f;
-		if (specularStrength > 5.0f)
-			specularStrength = 5.0f;
-		std::cout << "Especular = " << specularStrength << std::endl;
+		specular_strength += 0.01f;
+		if (specular_strength > 5.0f)
+			specular_strength = 5.0f;
+		std::cout << "Especular = " << specular_strength << std::endl;
 	}
 	if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
 	{
-		specularStrength -= 0.01f;
-		if (specularStrength < 0.0f)
-			specularStrength = 0.0f;
-		std::cout << "Especular = " << specularStrength << std::endl;
+		specular_strength -= 0.01f;
+		if (specular_strength < 0.0f)
+			specular_strength = 0.0f;
+		std::cout << "Especular = " << specular_strength << std::endl;
 	}
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow* window, const int width, const int height)
 {
 	// make sure the viewport matches the new window dimensions; note that width and
 	// height will be significantly larger than specified on retina displays.
@@ -221,83 +221,81 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 }
 
 // glfw: whenever the mouse moves, this callback is called
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+void mouse_callback(GLFWwindow* window, const double xpos, const double ypos)
 {
-	if (firstMouse)
+	if (first_mouse)
 	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
+		last_x = xpos;
+		last_y = ypos;
+		first_mouse = false;
 	}
 
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	const float xoffset = xpos - last_x;
+	const float yoffset = last_y - ypos; // reversed since y-coordinates go from bottom to top
 
-	lastX = xpos;
-	lastY = ypos;
+	last_x = xpos;
+	last_y = ypos;
 
 	camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+void scroll_callback(GLFWwindow*, double, const double yoffset)
 {
 	camera.ProcessMouseScroll(yoffset);
 }
 
-CustomObject load_custom_object(std::pair<std::string, std::string> fileNameAndTexture)
+custom_object load_custom_object(const std::pair<std::string, std::string>& file_name_and_texture)
 {
-	CustomObject customObject;
+	custom_object custom_object;
 
-	std::vector<float> vector = read_csv_file(fileNameAndTexture.first);
-	std::string textureFileName = fileNameAndTexture.second;
+	auto vector = read_csv_file(file_name_and_texture.first);
+	const auto texture_file_name = file_name_and_texture.second;
 
-	int vectorSize = vector.size();
-	float* vertices = new float[vectorSize];
-	for (size_t i = 0; i < vectorSize; i++)
-	{
+	const int vector_size = vector.size();
+	auto* const vertices = new float[vector_size];
+	for (size_t i = 0; i < vector_size; i++)
 		vertices[i] = vector[i];
-	}
 
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	unsigned int VBO, objVAO;
-	glGenVertexArrays(1, &objVAO);
-	glGenBuffers(1, &VBO);
+	unsigned int vbo, obj_vao;
+	glGenVertexArrays(1, &obj_vao);
+	glGenBuffers(1, &vbo);
 
-	glBindVertexArray(objVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vectorSize, vertices, GL_STATIC_DRAW);
+	glBindVertexArray(obj_vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vector_size, vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, VERTICE_DEFINITION * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, vertice_definition * sizeof(float), static_cast<void*>(0));
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, VERTICE_DEFINITION * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, vertice_definition * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, VERTICE_DEFINITION * sizeof(float), (void*)(6 * sizeof(float)));
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, vertice_definition * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 
-	glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, VERTICE_DEFINITION * sizeof(float), (void*)(9 * sizeof(float)));
+	glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, vertice_definition * sizeof(float), reinterpret_cast<void*>(9 * sizeof(float)));
 	glEnableVertexAttribArray(3);
 
-	if (textureFileName != "")
+	if (!texture_file_name.empty())
 	{
-		customObject.texture = load_object_texture(textureFileName);
-		customObject.drawTexture = true;
+		custom_object.texture = load_object_texture(texture_file_name);
+		custom_object.draw_texture = true;
 	}
 	else
-		customObject.drawTexture = false;
+		custom_object.draw_texture = false;
 
-	customObject.points = vectorSize / VERTICE_DEFINITION;
-	customObject.VAO = objVAO;
-	customObject.VBO = VBO;
+	custom_object.points = vector_size / vertice_definition;
+	custom_object.vao = obj_vao;
+	custom_object.vbo = vbo;
 
-	return customObject;
+	return custom_object;
 }
 
-unsigned int load_object_texture(std::string textureFileName)
+unsigned int load_object_texture(const std::string& texture_file_name)
 {
 	unsigned int texture;
 	glGenTextures(1, &texture);
@@ -308,16 +306,16 @@ unsigned int load_object_texture(std::string textureFileName)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	int width, height, nrChannels;
+	int width, height, nr_channels;
 	stbi_set_flip_vertically_on_load(1);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-	unsigned char* data = stbi_load(textureFileName.c_str(), &width, &height, &nrChannels, 0);
+	auto* const data = stbi_load(texture_file_name.c_str(), &width, &height, &nr_channels, 0);
 	if (data)
 	{
 		// PNG with transparent background
-		std::size_t isPng = textureFileName.find(".png");
-		if (isPng != std::string::npos)
+		const auto is_png = texture_file_name.find(".png");
+		if (is_png != std::string::npos)
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 		else
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
